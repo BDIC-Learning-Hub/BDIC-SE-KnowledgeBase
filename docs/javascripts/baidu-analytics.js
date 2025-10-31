09583e5feddebed5da0b3489d17e99fff72a0eeb
Mrle <3140578604@qq.com>
2025-10-31 16:42:07 +0800

    新增统计页面

---
+++ docs/javascripts/baidu-analytics.js
@@ -0,0 +1,8 @@
+// 百度统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6217f66b72fb78c311a17976c21eca5a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
